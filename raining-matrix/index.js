665e900f2cdafd17eceeb3a6e321263496563b03
MohitSingh7979 <mohitsinghatwork@gmail.com>
2024-06-01 08:17:23 +0530

    working, solved by chatgpt

--- raining-matrix/index.js
+++ raining-matrix/index.js
@@ -4,72 +4,76 @@ window.onload = function () {
     w,
     h;
   fitCanvas();
-};
 
-let chars = "gsagkdagjksahgak;shgksahg".split("");
+  let chars = "gsagkdagjksahgak;shgksahg".split("");
 
-class particle {
-  constructor(x, y, char, hasTail, lightness, speed) {
-    this.x = x;
-    this.y = y;
-    this.val = char;
-    this.col = col;
-    if (speed) {
-      this.speed = speed;
-      this.light = lightness;
-    } else {
-      this.speed = Math.random();
-      this.light = Math.random() * 50 + 25;
+  class Particle {
+    constructor(x, y, char, col, hasTail, lightness, speed) {
+      this.x = x;
+      this.y = y;
+      this.val = char;
+      this.col = col;
+      if (speed) {
+        this.speed = speed;
+        this.light = lightness;
+      } else {
+        this.speed = Math.random();
+        this.light = Math.random() * 50 + 25;
+      }
+      if (hasTail) {
+        this.tl = Math.floor(Math.random() * 17) + 3;
+        this.tail = [];
+        for (let i = 1, len = this.tl; i < len; i++) {
+          this.tail.push(
+            new Particle(
+              this.x,
+              this.y - i * 10,
+              chars[Math.floor(Math.random() * chars.length)],
+              `hsl(135, 100%, ${this.light}%)`,
+              false,
+              this.lightness,
+              this.speed
+            )
+          );
+        }
+      }
     }
-    if (hasTail) {
-      this.tl = Math.floor(Math.random() * 17) + 3;
-      this.tail = [];
-      for (let i = 1, len = this.tl; i < len; i++) {
-        this.tail.push(
-          new particle(
-            this.x,
-            this.y - i * 10,
-            chars[Math.floor(Math.random() * chars.length)],
-            "hsl(135, 100%, " + this.light + "%)",
-            false,
-            this.lightness,
-            this.speed
-          )
-        );
+    move() {
+      this.y += this.speed;
+      if (this.y >= h) {
+        this.y -= h;
+      }
+    }
+
+    show() {
+      c.font = "10px serif";
+      c.fillStyle = this.col;
+      c.fillText(this.val, this.x, this.y);
+
+      if (this.tail) {
+        for (let i = 0, len = this.tail.length; i < len; i++) {
+          this.tail[i].move();
+          this.tail[i].show();
+        }
       }
     }
   }
-  move() {
-    this.y + this.speed;
-    if (this.y >= h) {
-      this.y -= h;
-    }
+
+  let p = [];
+  for (let i = 0, len = 500; i < len; i++) {
+    p.push(
+      new Particle(
+        Math.random() * w,
+        Math.random() * h,
+        chars[Math.floor(Math.random() * chars.length)],
+        "rgb(28, 161, 82)",
+        true
+      )
+    );
   }
 
-  show() {
-    c.font = "10px serif";
-    c.fillStyle = this.col;
-    c.fillText(this.val, this.x, this.y);
-
-    for (let i = 0, len = this.tl - 1; i < len; i++) {
-      this.tail[i].move();
-      this.tail[i].show();
-    }
-  }
-}
-
-let p = [];
-for (let i = 0, len = 500; i < len; i++) {
-  p.push(
-    new particle(
-      Math.random() * w,
-      Math.random() * h,
-      chars[Math.floor(Math.random() * chars.length)],
-      "rgb(28, 161, 82)",
-      true
-    )
-  );
   function draw() {
+    c.clearRect(0, 0, w, h);
     for (let i = 0, len = p.length; i < len; i++) {
       p[i].move();
       p[i].show();
@@ -78,7 +82,7 @@ for (let i = 0, len = 500; i < len; i++) {
 
   function fitCanvas() {
     w = ctx.width = window.innerWidth;
-    h = ctx.width = window.innerHeight;
+    h = ctx.height = window.innerHeight;
   }
 
   function loop() {
@@ -88,4 +92,4 @@ for (let i = 0, len = 500; i < len; i++) {
   }
 
   window.requestAnimationFrame(loop);
-}
+};
